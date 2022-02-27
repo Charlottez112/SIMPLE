d69c4a6b39b41c9e8b7d354ce6754cff73bcf9c9
Vatsala Prasad <vatsala.prasad1998@gmail.com>
2022-02-27 18:10:36 -0500

    Update data.py

--- Dataset_Code/data.py
+++ Dataset_Code/data.py
@@ -3,24 +3,18 @@ import glob
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-
 def custom_collate(data):
-    l = len(data)
-    state = np.zeros( (l , 60, 4096, 6))
-    t = np.zeros( (l , 1) )
-    i = 0
-    for item in data:
-        state[i , :, :, 0:3] = item["position"]
-        state[i , :, :, 3:6] = item["velocity"]
-        t[i, 0] = item["temperature"]
-        i+=1
+    position = np.stack([item["position"] for item in data])
+    velocity = np.stack([item["velocity"] for item in data])
+    temp = np.stack([item["temperature"] for item in data])
         
-    state = torch.from_numpy(state)
-    t = torch.from_numpy(t)
-    return {"position" : state[: , :, :, 0:3] , "velocity" : state[: , :, :, 3:6] , "temperature" : t}
+    position = torch.from_numpy(position)
+    velocity = torch.from_numpy(velocity)
+    temp = torch.from_numpy(temp)
+    return {"position" : position , "velocity" : velocity , "temperature" : temp}
 
 
-class LoadSimulation(Dataset):
+class Load_Simulation(Dataset):
     def __init__(self, n_particles = 4096, n_frame = 60):
         self.n_particles = n_particles
         self.n_frame = n_frame
