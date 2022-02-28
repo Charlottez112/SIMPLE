171e51a3666eb27f6ffc214f2d99f5bcc3ee6f08
Vatsala Prasad <vatsala.prasad1998@gmail.com>
2022-02-28 10:34:47 -0500

    Update data.py
    Added boxdim to the pipeline.

--- Dataset_Code/data.py
+++ Dataset_Code/data.py
@@ -7,13 +7,13 @@ def custom_collate(data):
     position = np.stack([item["position"] for item in data])
     velocity = np.stack([item["velocity"] for item in data])
     temp = np.reshape(np.stack([item["temperature"] for item in data]) , (-1,1))
-    config = np.reshape(np.stack([item["init_config"] for item in data]) , (-1,1)) 
+    dim = np.stack([item["boxdim"] for item in data])
         
     position = torch.from_numpy(position)
     velocity = torch.from_numpy(velocity)
     temp = torch.from_numpy(temp)
-    config = torch.from_numpy(config)
-    return {"position" : position , "velocity" : velocity , "temperature" : temp, "init_config" : config}
+    dim = torch.from_numpy(dim)
+    return {"position" : position , "velocity" : velocity , "temperature" : temp, "boxdim" : dim}
 
 
 class SimulationDataset(Dataset):
@@ -29,8 +29,8 @@ class SimulationDataset(Dataset):
     
     def __getitem__(self, idx):
         file_path = self.data[idx]
-        simulation = np.load(file_path)     
-        current_idx = {"position" : simulation['position'] , "velocity" : simulation['velocity']  , "temperature" : simulation['temperature'].item() , "init_config" : simulation['init_config']}
+        simulation = np.load(file_path)
+        current_idx = {"position" : simulation['position'] , "velocity" : simulation['velocity']  , "temperature" : simulation['temperature'].item() , "boxdim" : simulation['boxdim']}
         return current_idx
 
 
