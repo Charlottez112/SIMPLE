1d2ff1649659ea966cbda4e320263dae0b4aa242
Jae-Won Chung <jwnchung@umich.edu>
2022-02-27 20:18:02 -0500

    Implement loss functions

---
+++ simple/loss.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import torch
+
+
+def task_error(pred: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+    """A function that measures how accurate the state prediction is.
+
+    Args:
+        pred (Tensor): Prediction of the next state. Shape: [B, N, 6].
+        label (Tensor): Ground truth next state. Shape: [B, N, 6].
+    """
+    return torch.mean(torch.square(pred - label))
+
+
+def noether_loss_exact(quantities: list[torch.Tensor]) -> torch.Tensor:
+    """The Noether loss function that exactly conserves quantities.
+
+    Args:
+        quantities (list[Tensor]): List of quantity tensors, each of
+            shape [B, e].
+
+    The first item of the `quantities` list serves as the ground truth
+    quantity that all subsequent quantities must be similar to.
+    """
+    first_state_quantity = quantities[0]
+    later_quantities = torch.stack(quantities[1:])
+    return torch.mean(torch.square(later_quantities - first_state_quantity))
+
+
+def noether_loss_approx(quantities: list[torch.Tensor]) -> torch.Tensor:
+    """The Noether loss function that approximately conserves quantities.
+
+    Args:
+        quantities (list[Tensor]): List of quantity tensors, each of
+            shape [B, e].
+
+    Compares quantities of adjacent time steps and enforces that they
+    be similar.
+    """
+    quants = torch.stack(quantities)
+    return torch.mean(torch.square(quants[1:] - quants[:-1]))
