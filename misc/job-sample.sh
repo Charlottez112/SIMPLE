72f386b173d63456ecc2b7e53fb0c45925a53329
Dhiraj Maji <dmaji@umich.edu>
2022-03-04 09:32:14 -0500

    updated the job-sample.sh

--- misc/job-sample.sh
+++ misc/job-sample.sh
@@ -14,41 +14,14 @@
 cd ~/workspace/eecs545_project
 
 source ~/.local/miniconda3/etc/profile.d/conda.sh
-conda activate noether
+conda activate simple 
 
-python train.py \
-    --image_width 64 \
-    --g_dim 128 \
-    --z_dim 64 \
-    --dataset phys101 \
-    --data_root ./data/phys101/phys101/scenarios/ramp \
-    --tailor \
-    --num_trials 1 \
-    --n_past 2 \
-    --n_future 20 \
-    --num_threads 8 \
-    --ckpt_every 10 \
-    --inner_crit_mode mse \
-    --enc_dec_type vgg \
-    --emb_type conserved \
-    --num_epochs_per_val 1 \
-    --emb_dim 32 \
+python main.py \
+    --num_epochs 100 \
     --batch_size 4 \
-    --num_inner_steps 1 \
-    --num_jump_steps 0 \
-    --n_epochs 101 \
-    --train_set_length 311 \
-    --test_set_length 78 \
-    --inner_lr .0001 \
-    --val_inner_lr .0001 \
-    --outer_lr 0.0001 \
-    --outer_opt_model_weights \
-    --random_weights \
-    --only_twenty_degree \
-    --frame_step 2 \
-    --center_crop 1080 \
-    --num_emb_frames 2 \
-    --horiz_flip \
-    --batch_norm_to_group_norm \
-    --reuse_lstm_eps \
-    --log_dir ./results/phys101/ramp-$(date +%Y%m%d%H%M%S)/
+    --outer_lr 1e-4 \
+    --inner_lr 1e-4 \
+    --num_workers 1 \
+    --num_neighbors 10 \
+    --conserve_quantity 'approx' \
+    --device 'cuda'
